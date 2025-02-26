52a48f27ef5e1dd845efa6e4977d23e7cadab63e
ARIEL_RODRIGUEZ_CENICEROS <a.22308051280706@cbtis128.edu.mx>
2025-02-26 20:10:47 +0000

    ejemplos

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,62 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MisIconosApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MisIconosApp extends StatelessWidget {
+  const MisIconosApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Ariel Rodriguez"),
+          titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+          centerTitle: true,
+          backgroundColor: Colors.pink,
+        ),
+        body: Column(
+          children: <Widget>[
+            SizedBox(height: 20), // Espacio entre el AppBar y el texto
+            Text(
+              "Ariel Rodriguez Ceniceros 22308051280706",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.black,
+              ),
+              textAlign: TextAlign.center, // Centrar el texto
+            ),
+            SizedBox(height: 20), // Espacio entre el texto y los iconos
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Icon(
+                  Icons.person, // Icono de usuario
+                  color: Colors.pink,
+                  size: 40.0,
+                  semanticLabel: 'Text to announce in accessibility modes',
+                ),
+                Icon(
+                  Icons.audiotrack,
+                  color: Colors.green,
+                  size: 40.0,
+                ),
+                Icon(
+                  Icons.beach_access,
+                  color: Colors.blue,
+                  size: 40.0,
+                ),
+                Icon(
+                  Icons.location_on, // Icono de ubicación
+                  color: Colors.red, // Color rojo para el ícono de ubicación
+                  size: 40.0,
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
-  }
-}
+  } // fin de widgets
+} // fin de la clase MisIconosApp
